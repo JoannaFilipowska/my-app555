399d8eed6912168880b2544e5ed34508d1cbf535
Joanna Filipowska <j.filipowska@gmail.com>
2019-01-28 20:06:29 -0600

    adding bootstrap exercise

---
+++ src/components/Navigation.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+class Navigation extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/">
+          World Cuisine Recipes
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <form className="form-inline my-2 my-lg-0">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button
+            className="btn btn-outline-success my-2 my-sm-0"
+            type="submit"
+          >
+            Search
+          </button>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <button type="button" class="btn btn-light">
+            History of Chicken
+          </button>
+        </form>
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
